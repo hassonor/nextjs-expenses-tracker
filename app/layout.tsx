3fe4e4b530f21ad79cdd432cef50e8a3e535f32f
Or Hasson <hassonor@gmail.com>
2024-08-06 16:41:44 +0300

    save the user after first signup from clerk to neon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs';
+import Header from "@/components/Header";
 
 const roboto = Roboto({ weight:'400',subsets: ["latin"] });
 
@@ -18,7 +19,13 @@ export default function RootLayout({
   return (
       <ClerkProvider>
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+      <Header/>
+      <main className="container">
+      {children}
+
+      </main>
+      </body>
     </html>
         </ClerkProvider>
   );
