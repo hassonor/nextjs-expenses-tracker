4025af6c7b4c5e1b014978989e4c4adeee07877f
Or Hasson <hassonor@gmail.com>
2024-08-06 16:10:40 +0300

    setup clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import {ClerkProvider} from '@clerk/nextjs';
 
 const roboto = Roboto({ weight:'400',subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+      <ClerkProvider>
     <html lang="en">
       <body className={roboto.className}>{children}</body>
     </html>
+        </ClerkProvider>
   );
 }
